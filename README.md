2ecb4813318160b77b948dbc392c57ef9ef198a2
Rico Moorman <rico.moorman@gmail.com>
2024-02-08 01:35:12 +0100

    Add first bare version of new config
    * Following along <https://www.youtube.com/watch?v=zHTeCSVAFNY&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn&index=1>
    * Add `lazy` package manager
    * Add a theme, `telescope`, and `treesitter` along with a bit of
      configuration

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-
-```
+```bash
 $ REPO_PATH=$(pwd) # path to this repository on disk
 $ NVIM_APPNAME=nvim-gepiel # unique id for `~/.config/` directory
 
 $ # link the config
 $ ln -s "$REPO_PATH" "$HOME/.config/$NVIM_APPNAME"
 
-$ # run nvim with config
+$ # when using `direnv`, write to `.env` file
+$ echo NVIM_APPNAME=$NVIM_APPNAME > .env
+
+$ # not using `direnv`? set the `NVIM_APPNAME` environment variable manually
 $ export NVIM_APPNAME
+
+$ # running vim should now pick up the custom config
 $ vim
 ```
--- init.lua
+++ init.lua
@@ -3,3 +3,55 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- Basic tab settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+
+
+-- Make sure lazy package manager is installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+-- Setup packages
+local plugins = {
+  {"catppuccin/nvim", name = "catppuccin", priority = 1000},
+  {"nvim-telescope/telescope.nvim", tag = "0.1.5", dependencies = {"nvim-lua/plenary.nvim"}},
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+}
+local opts = {}
+require("lazy").setup(plugins, opts)
+
+--- theme
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
+
+--- telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+--- treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = "all",
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+
