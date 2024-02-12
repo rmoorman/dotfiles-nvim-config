e0bbea29a35a8f32b98c5272201f2d69b760753c
Rico Moorman <rico.moorman@gmail.com>
2024-02-12 23:52:17 +0100

    Cleanup

---
+++ lua/local-options.lua
@@ -0,0 +1,14 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+
+-- Basic tab settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+
