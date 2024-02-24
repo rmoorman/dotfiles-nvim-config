1fcd9f1f9b3fa72e52442fa50ba9e82aaef43954
Rico Moorman <rico.moorman@gmail.com>
2024-02-24 04:15:49 +0100

    Cleanup

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,17 +1,11 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-  },
-  {
     "navarasu/onedark.nvim",
     priority = 1000,
     opts = {
       style = "warmer",
     },
   },
-  { "morhetz/gruvbox" },
   {
     "nvim-lualine/lualine.nvim",
     opts = {
